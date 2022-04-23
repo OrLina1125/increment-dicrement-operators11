b4affa9de9dec99f61ab677dc0a5b3302d788913
Orlina M Brabar <90068655+OrLina1125@users.noreply.github.com>
2022-04-23 10:55:48 +0900

    add comparison operators

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+<body>
+
+<?php 
+//Comparison Operators
+//$x = 10;
+//echo $x++;
+//echo $x;
+?>
+
+<?php
+//Comparison Operators
+$x = 10;
+echo --$x;
+echo $x;
+
+?>
+
+
+</body>
+</html>
+
